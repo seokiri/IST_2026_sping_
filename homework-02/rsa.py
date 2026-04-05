2e654fb4f69dfdf7079359eed6a48ee4dbeb1899
seokiri <seokirini@gmail.com>
2026-04-05 17:32:47 +0300

    Update rsa.py

--- homework-02/rsa.py
+++ homework-02/rsa.py
@@ -13,8 +13,12 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+    if n < 2:
+      return False
+    for i in range(2, int(n**0.5)+1):
+      if n % i == 0:
+        return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:
@@ -26,8 +30,9 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while b:
+      a , b = b, a % b
+    return a
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -38,8 +43,20 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    # PUT YOUR CODE HERE
-    pass
+    d, x1, x2 = 0, 0, 1
+    y1, y2 = 1, 0
+    original_phi = phi
+    while e > 0:
+      q = phi // e
+      r = phi % e
+      phi, e = e, r
+      x = x1 - q * x2
+      y = y1 - q * y2
+      x1, x2 = x2, x
+      y1, y2 = y2, y
+    if x1 < 0:
+      x1 += original_phi
+    return x1
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -48,26 +65,16 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
-    # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
+    phi = (p-1)(q-1)
 
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
-
-    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
-
-    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
-    g = gcd(e, phi)
+    g = rsa.gsd(e, phi)
     while g != 1:
-        e = random.randrange(1, phi)
-        g = gcd(e, phi)
+      e = random.randrange(1, phi)
+      g = rsa.gsd(e, phi)
 
-    # Use Extended Euclid's Algorithm to generate the private key
-    d = multiplicative_inverse(e, phi)
-
-    # Return public and private keypair
-    # Public key is (e, n) and private key is (d, n)
+    d = rsa.multiplicative_inverse(e, phi)
     return ((e, n), (d, n))
 
 
